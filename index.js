fc8a48601a7daf089b3916dc58818678de191c7b
Julian Rodriguez <julian.rodriguez@amadeus.com>
2024-02-01 16:14:02 -0500

    Feat: Add option to remove octal escape sequences

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const { promises: fs } = require('fs')
 const main = async () => {
   const path = core.getInput('path')
   const trim = core.getBooleanInput('trim')
+  const containsOctalEscapes = core.getBooleanInput('containsOctalEscapes')
   let content = await fs.readFile(path, 'utf8')
   if (trim) {
     content = content.trim()
+  }
+  if (containsOctalEscapes) {
+    const regex = /\\[0-7]{1,3}/gm;
+    content = content.replace(regex, 'OCTAL_ESCAPE')
   }
 
   core.setOutput('content', content)
